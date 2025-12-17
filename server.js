993bc3d28cf56cf8a400eb78668b0e387b5cc460
yzed2000a-coder <yzed2000a@gmail.com>
2025-12-17 15:42:19 +0300

    Add fun image to homepage

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-console.log("Starting server...");
-
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve files in "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for homepage
 app.get('/', (req, res) => {
-    res.send('Hello! Test server running.');
+  res.send(`
+    <h1>Hello! Test server running.</h1>
+    <img src="/fun.jpg" alt="Fun picture" width="300">
+  `);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
